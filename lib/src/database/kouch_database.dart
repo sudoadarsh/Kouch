a8e51e1607e696bcad099f45b0fb0f00b8ef6196
sudoadarsh <sudoadarsh@gmail.com>
2023-10-31 19:16:31 +0530

    feat added: database delete.

--- lib/src/database/kouch_database.dart
+++ lib/src/database/kouch_database.dart
@@ -12,6 +12,9 @@ abstract class _KouchDatabase {
   /// * [replicas] : The number of copies of the database in the cluster. The default is 3, unless overridden in the cluster config .
   /// * [partitioned] : Whether to create a partitioned database. Default is false.
   Future<void> create({int shards, int replicas, bool partitioned});
+
+  /// Deletes the database and all the documents and attachments contained within it.
+  Future<void> delete();
 }
 
 class KouchDatabase implements _KouchDatabase {
@@ -39,10 +42,22 @@ class KouchDatabase implements _KouchDatabase {
       Uri.parse("$host/$name").replace(queryParameters: query),
       headers: auth.authHeaders(),
     );
-    // Validate the request.
+    // Validate the response.
     if (response.statusCode == 200 || response.statusCode == 201) {
       return; // Success.
     }
     throw KouchDatabaseException(response.body);
+  }
+
+  @override
+  Future<void> delete() async {
+    // Create the request.
+    final http.Response response = await http.delete(
+      Uri.parse("$host/$name"),
+      headers: auth.authHeaders(),
+    );
+    // Validate the response.
+    if (response.statusCode != 200) throw KouchDatabaseException(response.body);
+    return; // Success.
   }
 }
